3d9962876763c1fe7c4af2b833c0f31df7855c32
Luke Channings <luke@channings.me>
2020-05-02 15:49:00 +0100

    Update lint script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e -o pipefail
+
 if ! [ -d .git ]; then
   echo "Lint should be run from the project root."
   exit 1
@@ -9,8 +11,6 @@ fi
 if ! [ -L .git/hooks/pre-commit ]; then
   ln -s "$(pwd)/scripts/lint.sh" .git/hooks/pre-commit
 fi
-
-set -e -o pipefail
 
 yamllint -c .yamllint.yaml .
 
