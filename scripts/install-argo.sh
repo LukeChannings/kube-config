b44ebb7a349398190e49455b34ddc2e5249f8200
Luke Channings <luke@channings.me>
2020-05-06 21:17:10 +0100

    Move argocd values into apps structure.

--- scripts/install-argo.sh
+++ scripts/install-argo.sh
@@ -5,32 +5,8 @@ set -exu -o pipefail
 helm repo add argo https://argoproj.github.io/argo-helm
 kubectl create ns argocd || true
 
-cat > argo-values.yaml << EOF
----
-installCRDs: false
-controller:
-  metrics:
-    enabled: true
-server:
-  ingress:
-    enabled: true
-    annotations:
-      cert-manager.io/cluster-issuer: letsencrypt-production
-      ingress.class: internal
-      haproxy.org/ssl-redirect: true
-      haproxy.org/ssl-passthrough: true
-    hosts:
-      - argo.private.channings.me
-    tls:
-      - hosts:
-          - argo.private.channings.me
-        secretName: argocd-secret
-EOF
-
 # https://github.com/argoproj/argo-helm/tree/master/charts/argo-cd
-helm upgrade -i -f argo-values.yaml argo -n argocd argo/argo-cd
-
-rm argo-values.yaml
+helm upgrade -i -f ./apps/infrastructure/argocd/values.yaml argo -n argocd argo/argo-cd
 
 PASSWORD="$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2)"
 
@@ -39,7 +15,7 @@ Username: admin
 Password: ${PASSWORD}
 "
 
-argocd login 192.168.1.201 --username admin --password "$PASSWORD"
+argocd login argo.private.channings.me --username admin --password "$PASSWORD"
 
 argocd repo add git@github.com:LukeChannings/k8s-at-home.git --insecure-ignore-host-key --ssh-private-key-path ~/Projects/k8s-at-home/secrets/argocd-github
 
