ad1cb627e4996e8c5c2e77d50a8432dc868fb82c
Luke Channings <luke@channings.me>
2020-05-07 14:25:08 +0100

    Script for writing crds based on cluster's registered CRDs.

--- scripts/write-crds.js
+++ scripts/write-crds.js
@@ -1,38 +1,17 @@
 #!/usr/bin/env node
 
-/**
- * Usage: kubectl get crd -A -o json | ./scripts/write-crds.js
- */
-
+const { spawnSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-const allCrdsPromise = new Promise((resolve, reject) => {
-  let stdin = '';
+const crdCmd = spawnSync('kubectl', ['get', 'crd', '-A', '-o', 'json'], { encoding: 'utf-8', maxBuffer: 1024 * 8192 })
 
-  process.stdin.setEncoding('utf8');
-
-  process.stdin.on('readable', () => {
-    let chunk;
-    while ((chunk = process.stdin.read()) !== null) {
-      stdin += chunk;
-    }
-  });
-
-  process.stdin.on('end', () => {
-    try {
-      resolve(JSON.parse(stdin.trim()));
-    } catch (err) {
-      reject(new Error("Stdin was no a JSON document! Use `kubectl get crd -A -o json`."))
-    }
-  });
-})
-
-const writeCrd = crd => {
-  const filename = `${crd.spec.names.singular}-${crd.spec.group.split('.')[0]}-${crd.spec.versions[0].name}.json`
-  fs.writeFileSync(path.resolve(__dirname, '../crds/master-standalone', filename), JSON.stringify(crd, null, 2));
+if (crdCmd.status === 0) {
+  JSON.parse(crdCmd.stdout).items.forEach(crd => {
+    const filename = `${crd.spec.names.singular}-${crd.spec.group.split('.')[0]}-${crd.spec.versions[0].name}.json`
+    console.log(`Writing ${filename}`)
+    fs.writeFileSync(path.resolve(__dirname, '../crds/master-standalone', filename), JSON.stringify(crd, null, 2));
+  })
+} else {
+  console.error('ERROR', crdCmd.stderr)
 }
-
-allCrdsPromise
-  .then(allCrds => allCrds.items.forEach(writeCrd))
-  .catch(err => console.error(err))
