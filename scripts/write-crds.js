563ce39e455be99318d0c38c91bb3671c8b535d1
Luke Channings <luke@channings.me>
2021-06-24 17:24:20 +0100

    Update CRDs and locks

--- scripts/write-crds.js
+++ scripts/write-crds.js
@@ -10,7 +10,7 @@ if (crdCmd.status === 0) {
   JSON.parse(crdCmd.stdout).items.forEach(crd => {
     const filename = `${crd.spec.names.singular}-${crd.spec.group.split('.')[0]}-${crd.spec.versions[0].name}.json`
     console.log(`Writing ${filename}`)
-    fs.writeFileSync(path.resolve(__dirname, '../crds/master-standalone', filename), JSON.stringify(crd, null, 2));
+    fs.writeFileSync(path.resolve(__dirname, '../crds/v1.20.6-standalone', filename), JSON.stringify(crd, null, 2));
   })
 } else {
   console.error('ERROR', crdCmd.stderr)
